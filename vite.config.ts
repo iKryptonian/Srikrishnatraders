05260309fb6e67d79baf262121e1a2136d40c334
shadow <nivash0022@gmail.com>
2025-01-31 10:16:41 +0530

    eighth commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Srikrishnatraders' // Ensure correct repo name in GitHub Pages
+  base: "" ,// Ensure correct repo name in GitHub Pages
+  plugins: [react()]
+  
 });
