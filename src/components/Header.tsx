b4d9e180f51f13779c2d3ff911aeeb17d823ef6f
shadow <nivash0022@gmail.com>
2025-02-06 17:48:26 +0530

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,7 +32,7 @@ export const Header: React.FC<HeaderProps> = ({ cart }) => {
           className="flex items-center space-x-2 text-white hover:text-yellow-200"
         >
           <img
-            src="/Srikrishnatraders/public/sklogo.jpg"
+            src="/sklogo.jpg"
             alt="Sri Krishna Traders"
             className="h-14 w-auto"
           />
