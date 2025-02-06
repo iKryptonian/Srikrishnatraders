9a6b35ba9ec087571c9329d507c92ac87e8cfabb
shadow <nivash0022@gmail.com>
2025-02-06 07:58:58 +0530

    tenth commit works

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { HomePage } from './pages/HomePage';
 import { ProductsPage } from './pages/ProductsPage';
@@ -12,21 +12,18 @@ function App() {
   const { cart, addToCart, removeFromCart } = useCart();
 
   return (
-    <Router basename="/Srikrishnatraders/">
-      <div className="min-h-screen bg-gray-100">
-        <Header cart={cart} onRemoveFromCart={removeFromCart} />
-
-        <main className="pt-20 container mx-auto px-4">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/products" element={<ProductsPage onAddToCart={addToCart} />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/diwali-chit" element={<DiwaliChitPage />} />
-            <Route path="/cart" element={<CartPage cart={cart} onRemoveFromCart={removeFromCart} />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-gray-100">
+      <Header cart={cart} onRemoveFromCart={removeFromCart} />
+      <main className="pt-20 container mx-auto px-4">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage onAddToCart={addToCart} />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/diwali-chit" element={<DiwaliChitPage />} />
+          <Route path="/cart" element={<CartPage cart={cart} onRemoveFromCart={removeFromCart} />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
