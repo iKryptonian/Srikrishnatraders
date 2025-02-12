72ac88fa81cf67460e4ce0ca6fc7a92b97651fd8
shadow <nivash0022@gmail.com>
2025-02-12 13:03:11 +0530

    commit02

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,16 +24,16 @@ export const Header: React.FC<HeaderProps> = ({ cart }) => {
   };
 
   return (
-    <header className="bg-gradient-to-r from-orange-400 to-blue-400 p-4 fixed w-full top-0 z-50">
+    <header className="bg-gradient-to-r from-orange-400 to-blue-400 p-3 fixed w-full top-0 z-50">
       <div className="container mx-auto flex items-center justify-between">
         <Link
           to="/"
           className="flex items-center space-x-2 text-white hover:text-yellow-200"
         >
           <img
-            src="https://ikrytonian.sirv.com/sklogo.jpg"
+            src="https://ikrytonian.sirv.com/sktlogo1.png"
             alt="Sri Krishna Traders"
-            className="h-14 w-auto"
+            className="h-20 w-auto"
           />
         </Link>
 
